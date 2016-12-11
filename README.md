4fa97a1aca297bc3bf97d5e83b030007903a4dfc
Joel Bryan Juliano <joelbryanjuliano@altus-dc.com>
2016-12-12 03:12:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Debride.tmbundle
+================
+
+Debride (https://github.com/seattlerb/debride) Textmate 2 Bundle
+
+(c) 2017 Joel Bryan Tiam Juliano joelbryan.juliano@gmail.com
+
+Supports
+--------
+* analysing dead and uncalled methods
+* different icons for analysis
+
+Install
+-------
+
+Make sure $TM_RUBY is set to current ruby directory, then:
+
+    gem install debride
+    mkdir -p ~/Library/Application\ Support/TextMate/Bundles
+    cd ~/Library/Application\ Support/TextMate/Bundles
+    git clone https://github.com/jjuliano/Debride.tmbundle.git
+
+Screenshot
+----------
+![screenshot](Support/screenshot.png?raw=true "screenshot")
+
+Credits
+-------
+Emoji provided free by http://emojione.com
---
+++ Support/debride.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+
+begin
+  require 'debride'
+  require 'json'
+rescue LoadError
+  puts "Install debride!\ngem install debride"
+  exit 1
+end
+
+def log(msg)
+  require 'logger'
+  logger = Logger.new('/tmp/debride_bundle.log')
+  logger.info msg
+end
+
+def offences(file)
+  @bad = []
+  debride = Debride.run([file])
+  debride.missing.each do |klass, meths|
+    bad = meths.map { |meth|
+      location = debride.method_locations["#{klass}##{meth}"]
+      line = location[/.*:(\d+)$/, 1]
+      path = location[/(.+):\d+$/, 1]
+      [location]
+    }
+    bad.compact!
+    next if bad.empty?
+    @bad = bad
+  end
+  @bad
+end
+
+def messages(offences)
+  messages = {
+    warning: {}
+  }
+  offences.each do |offence|
+    severity = :warning
+    line = offence[0][/.*:(\d+)$/, 1]
+    message = messages[severity][line] ||= []
+    message << "Thi method MIGHT not be called."
+  end
+  messages
+end
+
+def command(messages)
+  icons = {
+    warning:    "#{ENV['TM_BUNDLE_SUPPORT']}/bride.png".inspect
+  }
+  args = []
+
+  messages.each do |severity, messages|
+    args << ["--clear-mark=#{icons[severity]}"]
+    messages.each do |line, message|
+      args << "--set-mark=#{icons[severity]}:#{message.uniq.join('. ').inspect}"
+      args << "--line=#{line}"
+    end
+  end
+
+  args << ENV['TM_FILEPATH'].inspect
+
+  "#{ENV['TM_MATE']} #{args.join(' ')}"
+end
+
+cmd = command(messages(offences(ENV['TM_FILEPATH'])))
+
+# log cmd
+exec cmd
